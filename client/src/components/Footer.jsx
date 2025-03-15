23317f6ad27439303315e8990216a5411052bb46
BM-ACADEMY <teamkamarweb@gmail.com>
2025-03-15 12:03:36 +0530

    year changed in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,10 +4,11 @@ import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
+    var date=new Date().getFullYear();
   return (
     <footer className='border-t'>
         <div className='container mx-auto p-4 text-center flex flex-col lg:flex-row lg:justify-between gap-2'>
-            <p>All Rights Reserved © <a href="https://bmtechx.in/" target='_block'> BMtechx.in</a> 2024.</p>
+            <p>All Rights Reserved © <a href="https://bmtechx.in/" target='_block'> BMtechx.in</a> {date}.</p>
 
             {/* <div className='flex items-center gap-4 justify-center text-2xl'>
                 <a href='' className='hover:text-primary-100'>
